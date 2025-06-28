29276f940c7590850355de26c07b780d615863e0
boyarkogc <boyarko.greg@gmail.com>
2025-06-28 11:23:29 -0600

    initial updates to make this my own site

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`Hey! I'm Greg Boyarko, a full stack software engineer 
+        who loves to connect the internet to reality. 
+        This portfolio showcases my projects, blog posts, 
+        and professional journey. 
+        Feel free to explore and connect with me!`}
       </p>
       <div className="my-8">
         <BlogPosts />
