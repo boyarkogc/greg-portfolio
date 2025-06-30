e06e8acd4a43fdf629e1cd6406adc03bcb3678d7
boyarkogc <boyarko.greg@gmail.com>
2025-06-30 17:44:37 -0600

    Added a link to one of my projects

---
+++ app/projects/page.tsx
@@ -0,0 +1,19 @@
+export default function Projects() {
+  return (
+    <section>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Projects</h1>
+      <ul>
+        <li>
+          <a 
+            href="https://syncseed.app" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="text-blue-600 hover:text-blue-800 underline"
+          >
+            syncseed.app
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
